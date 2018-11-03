e987edfa1d2cb0a17e934444f7d32a475284724a
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-03 17:45:12 -0600

    add file with notes

--- spec/requests/api/v1/forecasts/forecast_search_request_spec.rb
+++ spec/requests/api/v1/forecasts/forecast_search_request_spec.rb
@@ -4,9 +4,9 @@ describe 'Forecast API' do
   it 'returns weather by zip_code' do
     zip_code = "80215"
 
-    get "/api/v1/forecasts/search?zip_code=#{zip_code}"
+    # get "/api/v1/forecasts/search?zip_code=#{zip_code}"
 
-    
+
 
 
   end
