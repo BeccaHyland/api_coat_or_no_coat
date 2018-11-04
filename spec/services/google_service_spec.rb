4f53aa035599829f3601ee4ee72f00d6957592f5
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-03 19:01:18 -0600

    google service #geocode_search returns json for lat/long when given zip_code

--- spec/services/google_service_spec.rb
+++ spec/services/google_service_spec.rb
@@ -3,18 +3,15 @@ require 'rails_helper'
 describe GoogleService do
   subject { GoogleService.new("80215") }
 
-  it 'exists' do
-    expect(subject).to be_a(GoogleService)
-  end
-
-  context 'geocoding data' do
+  context '#geocode_search' do
     context 'with valid zip code' do
       it "returns geocoded latitude and longitude" do
-      results = subject.geocoded_data
 
-      expect(results.first).to have_key[:lat]
-      expect(results.first).to have_key[:lng]
+        results = subject.geocode_search
+
+        expect(results).to have_key(:lat)
+        expect(results).to have_key(:lng)
+      end
     end
   end
-
 end
