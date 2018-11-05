57670ccdf7b5da7150d0eef3195bb85d99c2c35f
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-05 10:54:50 -0700

    update forecast to return daily, not hourly data, add attributes to forecast

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,5 +1,14 @@
 class Forecast
-  def initialize(data = {})
-    @data = data
+  attr_reader :summary,
+              :precip_probability,
+              :precip_type,
+              :max_precip_time
+
+  def initialize(json_data = {})
+    @summary = json_data[:summary]
+    @precip_probability = json_data[:precipProbability]
+    @precip_type = json_data[:precipType]
+    @max_precip_time = json_data[:precipIntensityMaxTime] #format this to hour out of unix stamp
   end
+
 end
--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 describe Forecast do
-  json_data = {}
+  json_data = {summary: "Snow in the afternoon",
+                precip_probability: 0.67,
+                precip_type: "snow",
+                max_precip_time: "1541455200"}
+                #is above formatted correctly?
   subject { Forecast.new(json_data) }
 
-  it 'exists' do
+  it 'exists with attributes' do
     expect(subject).to be_a(Forecast)
+    expect(subject.summary).to eq("Snow in the afternoon")
   end
 
 end
