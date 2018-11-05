1c37ea55fed7836a85cbb63f59a9376dfde91d23
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-05 12:37:45 -0700

    simplify variable

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe Forecast do
-  json_data = {summary: "Snow in the afternoon",
-                precip_probability: 0.67,
-                precip_type: "snow",
-                max_precip_time: "1541455200"}
-                #is above formatted correctly?
-  subject { Forecast.new(json_data) }
+  data = { summary: "Snow in the afternoon",
+          precip_probability: 0.67,
+          precip_type: "snow",
+          max_precip_time: "1541455200" }
+
+  subject { Forecast.new(data) }
 
   it 'exists with attributes' do
     expect(subject).to be_a(Forecast)
