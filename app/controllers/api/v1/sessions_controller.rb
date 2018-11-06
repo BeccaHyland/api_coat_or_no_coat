e56aa211690bd1d3420d86722280358dc991d0b3
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-06 15:28:37 -0700

    assigns sessions the user id when session or user created

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by_email(session_params[:email])
     if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
       render json: UserSerializer.new(user), status: 200
     else
       render json: {}, status: 400, message: "Oops, something went wrong. Please try again."
