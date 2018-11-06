cc5156e83d9c592ff20c19e81c7bfe222cb58b20
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-06 10:48:30 -0700

    add validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 describe User, type: :model do
+  context 'validaitons' do
+    it {should validate_presence_of :email}
+    it {should validate_uniqueness_of :email}
+    it {should validate_presence_of :password_digest}
+    it {should validate_presence_of :zip_code}
+    it {should validate_presence_of :phone_number}
+  end
 
 end
