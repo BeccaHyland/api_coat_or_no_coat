52aa31119ae95b3dff87fe4dc8c8dc1236c1ec41
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-11-06 15:15:29 -0700

    adds test for session create sad path

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -4,7 +4,6 @@ describe 'Sessions API' do
   describe 'POST /api/v1/sessions' do
     context 'with valid email and username' do
       it 'creates a session for the user' do
-
         user = create(:user)
 
         headers = { "Content-Type" => "application/json",
@@ -19,6 +18,21 @@ describe 'Sessions API' do
         post_response = JSON.parse(response.body, symbolize_names: true)
 
         expect(post_response[:data][:attributes][:api_key]).to eq(user.api_key)
+      end
+    end
+
+    context 'with invalid email / username combination' do
+      it 'returns a 400 status' do
+        user = create(:user)
+
+        headers = { "Content-Type" => "application/json",
+                    "Accept" => "application/json"}
+        json_payload = { email: "kitten@kitten.com",
+                          password: "big_kitten"}.to_json
+
+        post "/api/v1/sessions", params: json_payload, headers: headers
+
+        expect(response.status).to eq(400)
       end
     end
   end
